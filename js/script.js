7d415b450f6f7e27e74f85a1a47e0506e76a4082
fernandanevesf <67206016+fernandanevesf@users.noreply.github.com>
2023-09-01 09:33:32 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+async function getData(){
+    const requestURL = "data.json";
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const summaryEl= await response.json();
+
+    fillSummary(summaryEl);
+}
+
+function fillSummary(obj){
+    const elements = obj.summary;
+
+    const img = document.querySelectorAll("img");
+    const category = document.querySelectorAll(".category");
+    const score = document.querySelectorAll(".score");
+
+    for( const el of elements){
+        img.src = `${el.icon}`
+        category.textContent = `${el.category}`;
+        score.textContent = `${el.score}`;
+    }
+    
+}
+
+getData();
